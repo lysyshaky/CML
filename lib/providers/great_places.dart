3219b77e87447ea0495a924c7e53f9ef9d860406
Woloyor <yuralysyshak@gmail.com>
2023-03-24 12:38:49 +0200

    sql db

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
-import 'package:cml/models/place.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -10,17 +12,37 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
-  void addPlace(String pickedTitle, File pickedImage) {
+  void addPlace(
+    String pickedTitle,
+    File pickedImage,
+  ) {
     final newPlace = Place(
-        id: DateTime.now().toString(),
-        image: pickedImage,
-        title: pickedTitle,
-        location: PlaceLocation(
-          latitude: 37.422,
-          longitude: -122.084,
-          address: 'San Francisco, CA',
-        ));
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: pickedTitle,
+      location: null,
+    );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,27 +1,53 @@
-import 'package:cml/screens/add_place_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+
+import './add_place_screen.dart';
+import '../providers/great_places.dart';
 
 class PlacesListScreen extends StatelessWidget {
-  const PlacesListScreen({key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Places'),
+        title: Text('Your Places'),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.add),
+            icon: Icon(Icons.add),
             onPressed: () {
               Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
-              // Go to add place screen
             },
-          )
+          ),
         ],
       ),
-      body: Center(
-        child: const Text('Got no places yet, start adding some!'),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: const Text('Got no places yet, start adding some!'),
+                ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              greatPlaces.items[i].image,
+                            ),
+                          ),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            // Go to detail page ...
+                          },
+                        ),
+                      ),
+              ),
       ),
     );
   }
