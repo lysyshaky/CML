ad8e7b6ec9baa08103cfe4ff5afe18bad3ff1097
Woloyor <yuralysyshak@gmail.com>
2023-03-27 18:06:42 +0300

    location and camera

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const GOOGLE_API_KEY = 'AIzaSyC3pM2n6fA-7uiclIsGFwFp8pGw2b5Rfyc';
 
 class LocationHelper {
@@ -6,5 +9,12 @@ class LocationHelper {
     double longitude,
   }) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_API_KEY';
+    final response = await http.get(Uri.parse(url));
+    return json.decode(response.body)['results'][0]['formatted_address'];
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,30 @@
-import 'package:cml/providers/great_places.dart';
-import 'package:cml/screens/add_place_screen.dart';
-import 'package:cml/screens/places_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
+import './screens/place_detail_screen.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
       value: GreatPlaces(),
       child: MaterialApp(
-        title: 'Great Places',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-          accentColor: Colors.amber,
-        ),
-        home: PlacesListScreen(),
-        routes: {
-          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
-        },
-      ),
+          title: 'Great Places',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+            PlaceDetailScreen.routeName: (ctx) => PlaceDetailScreen(),
+          }),
     );
   }
 }
